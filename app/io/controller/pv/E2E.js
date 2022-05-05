9cea82868b46766f7c61d06c6f98166dd5a44fec
Ali Azmoodeh <aazmoodeh806@yahoo.com>
2022-05-06 00:06:47 +0430

    change onPvTyping

--- app/io/controller/pv/E2E.js
+++ app/io/controller/pv/E2E.js
@@ -2,4 +2,10 @@ let Server = require('app/io/connect/connection');
 
 Server.io.on('connection', (socket) => {
 
+    socket.on('onPvTyping', date => {
+        socket.broadcast.emit('isPvTyping', Json.builder({
+            'isPvTyping': date['isPvTyping']
+        }));
+    });
+
 });
