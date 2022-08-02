af10cd5cda5c888117677f6387202d67a9d2bbc8
Ali Azmoodeh <treeroot.ir@gmail.com>
2022-08-02 16:08:38 +0430

    fix bug

--- app/controller/user/personal/PersonalInterface.js
+++ app/controller/user/personal/PersonalInterface.js
@@ -424,9 +424,16 @@ exports.addMessage = (req) => {
 
         let phone = data.phoneNumber;
 
-        Insert.messageIntoUserSavedMessage(phone, message);
+        FindInUser.isSavedMessageCreated(phone, result => {
 
-        return Json.builder(Response.HTTP_CREATED);
+            if (!result)
+                return Json.builder(Response.HTTP_NOT_FOUND);
+
+            Insert.messageIntoUserSavedMessage(phone, message);
+
+            return Json.builder(Response.HTTP_CREATED);
+        });
+
     });
 
 }
@@ -450,9 +457,17 @@ exports.deleteMessage = (req) => {
         if (listOfId === undefined)
             return Json.builder(Response.HTTP_BAD_REQUEST);
 
-        Delete.itemInSavedMessage(phone, listOfId);
 
-        return Json.builder(Response.HTTP_OK);
+        FindInUser.isSavedMessageCreated(phone, result => {
+
+            if (!result)
+                return Json.builder(Response.HTTP_NOT_FOUND);
+
+            Delete.itemInSavedMessage(phone, listOfId);
+
+            return Json.builder(Response.HTTP_OK);
+
+        });
 
     });
 
@@ -467,9 +482,16 @@ exports.editMessage = (req) => {
 
         let phone = data.phoneNumber;
 
-        Update.itemInSavedMessage(phone, id);
 
-        return Json.builder(Response.HTTP_OK);
+        FindInUser.isSavedMessageCreated(phone, result => {
+
+            if (!result)
+                return Json.builder(Response.HTTP_NOT_FOUND);
+            Update.itemInSavedMessage(phone, id);
+
+            return Json.builder(Response.HTTP_OK);
+
+        });
 
     });
 
