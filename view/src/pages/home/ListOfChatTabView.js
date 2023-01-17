8e0a472a17e57ac775f29ce3b31986258eae70e4
Ali Azmoodeh <treeroot.ir@gmail.com>
2023-01-17 18:22:31 +0330

    fix some issue

--- view/src/pages/home/ListOfChatTabView.js
+++ view/src/pages/home/ListOfChatTabView.js
@@ -1,13 +1,19 @@
 import TabView from "component/TabView";
 import {useState} from "react";
 
-function Item() {
+function Item({img, _id, name, defaultColor}) {
     return (
         <li className="flex py-4 first:pt-0 last:pb-0">
-            <img className="h-10 w-10 rounded-full" src="{person.imageUrl}" alt=""/>
+            {
+                img ?
+                    <img className="h-10 w-10 rounded-full" src={img} alt="User Avatar"/> :
+                    <div className="h-10 w-10 rounded-full" style={{
+                        color: defaultColor
+                    }}>{name.slice(0, 2)}</div>
+            }
+            <img className="h-10 w-10 rounded-full" src={img} alt="User Avatar"/>
             <div className="ml-3 overflow-hidden">
-                <p className="text-sm font-medium text-slate-900">idfdjo</p>
-                <p className="text-sm text-slate-500 truncate">hskksd</p>
+                <p className="text-sm font-medium text-slate-900">{name}</p>
             </div>
         </li>
     )
@@ -17,8 +23,8 @@ export default function ListOfChatTabView() {
 
     const [columnSelected, setColumnSelected] = useState('All');
 
-    const handleColumnSelected = (d) => {
-        setColumnSelected(d);
+    const handleColumnSelected = d => {
+        setColumnSelected(d.toLowerCase());
     };
 
 
@@ -27,7 +33,6 @@ export default function ListOfChatTabView() {
             <TabView headers={['All', 'E2E', 'Group', 'Channel']} getColumnSelected={handleColumnSelected}/>
 
             <ul role="list" className="p-6 divide-y divide-slate-200">
-
 
 
             </ul>
